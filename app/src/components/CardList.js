1e32cb71b2295c4aeb617ff55844627eafba7642
biskoi <biskoi@daydream.cafe>
2020-03-01 21:34:27 -0700

    added state to cardlist

--- app/src/components/CardList.js
+++ app/src/components/CardList.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import data from './dummydata';
 import Card from './IssueCard';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 const Cardlist = styled.div`
 border: solid black 2px;
@@ -14,12 +16,26 @@ align-items: center;
 
 export default function CardList (props) {
 
+    const [cardData, setCardData]  = useState([]);
+
+    useEffect(() => {
+        axios.post('https://reqres.in/api/users', data)
+        .then(res => {
+            console.log(res);
+            setCardData(res.data);
+        })
+        .catch(err => console.log(err))
+    }, []);
+
     return (
         <Cardlist>
             <p>This box is the cardlist</p>
-            {data.map((item, index) => (
+            {cardData.map((item, index) => (
                 <Card data = {item} key = {index}/>
             ))}
+            <Link to = '/'>
+            <button>Home</button>
+            </Link>
         </Cardlist>
     )
 
