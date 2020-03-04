d4cd5d6513172402db0187f3da07459017242d37
apple <apple@apples-MacBook-Pro-2.local>
2020-03-04 09:52:00 -0800

    added delete,edit buttons, changehandler

--- app/src/components/IssueCard.js
+++ app/src/components/IssueCard.js
@@ -7,33 +7,90 @@ width: 30%;
 `;
 
 export default function IssueCard (props) {
-console.log(props)
+// console.log(props)
 const [isUser, setIsUser] = useState(false);
 const [isEditing, setIsEditing] = useState(false);
+const [data, setData] = useState(props.data);
+
+useEffect(() => {
+console.log(data)
+}, [data])
+
+
+
 
 useEffect(() => {
     if (props.data.creator_id == localStorage.getItem('user_id')){
-        console.log('able to edit or delete')
+        // console.log('able to edit or delete')
         setIsUser(true);
     } else {
-        console.log('NOT able to edit or delete')
+        // console.log('NOT able to edit or delete')
     }
 }, [])
+
+
 const editCard = () => {
     setIsEditing(!isEditing);
 }
+
+
+const changeHandler = event => {
+    setData({
+        ...data,
+        [event.target.name]: event.target.value
+    })
+    // console.log(event.target)
+}
+
+const deleteHandler = event => {
+    event.preventDefault();
+    console.log('deleted')
+}
+
     return (
         <Card>
-            <p>Created on: {props.data.created_at}</p>
+            {isEditing ? (
+            <>
+            <form>
+                <label>Card title:</label>
+                <input
+                    type='text'
+                    name='title'
+                    value={data.title}
+                    onChange={event => changeHandler(event)}
+                />
+
+                <label>Description:</label>
+                <input
+                    type='text'
+                    name='title'
+                    value={data.desc}
+                    onChange={event => changeHandler(event)}
+                />
+
+                <label>Issue Status:</label>
+                <button onClick={(event)=>{
+                    event.preventDefault();
+                    setData({
+                        ...data,
+                        'resolved': !data.resolved
+                    })
+                }}>{data.resolved ? ('Issue Resolved') : ('Not Resolved')}</button>
+                <button type='submit'>Submit Edit</button>
+
+            </form>
+            </>
+
+            ) : 
+            (<><p>Created on: {props.data.created_at}</p>
             <p>Created by: {props.data.creator_id}</p>
-            <h3>This is a card</h3>
-            <p>Card title: {props.data.title}</p>
+            <h3>Card title: {props.data.title}</h3>
             <p>Description: {props.data.desc}</p>
             <p>Upvotes: {props.data.upvotes}/Downvotes: {props.data.downvotes}</p>
-            <p>Issue Status: {props.data.resolved}</p>
-            {isUser && <button onClick={()=>{editCard()}} >
+            <p>Issue Status: {props.data.resolved}</p></>)}
+            {isUser && (<><button onClick={()=>{editCard()}} >
                 {isEditing ? ('Abort Edit') : ('edit')}
-            </button>}
+            </button><button onClick={event => deleteHandler(event)}>Delete</button></>)}
         </Card>
     );
 };
